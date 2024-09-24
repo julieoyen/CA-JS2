9a5f37878ed58b641eb69d687dadb2f121bf73ef
julieoyen <juloye51070@stud.noroff.no>
2024-09-24 13:24:22 +0200

    Improve login page functionality

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,28 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN, API_KEY } from "../constants";
+import { headers } from "../headers";
+
+export async function login({ email, password }) {
+  try {
+    const response = await fetch(API_AUTH_LOGIN, {
+      method: "POST",
+      headers: {
+        ...headers(),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      const errorText = `Login failed${response.statusText}(${response.status})`;
+      alert(`Error: ${errorText}. Please try again.`);
+      throw new Error(errorText);
+    }
+
+    const responseData = await response.json();
+    const accessToken = responseData.data.accessToken;
+    localStorage.setItem("accessToken", accessToken);
+    window.location.href = "/index.html";
+  } catch (error) {
+    console.error("Login error:", error);
+  }
+}
